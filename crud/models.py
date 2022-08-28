83a729504c934de35369d76e078d572c5a3f564b
Svet01 <sebastianvilte14@gmail.com>
2022-08-28 01:26:02 -0300

    jojos

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,16 @@
+import imp
+from pydoc import describe
 from django.db import models
+from django.contrib import admin
+from crud.views import task
 
-# Create your models here.
+class Task(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=108)
+    status = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+        
+admin.site.register(Task)
