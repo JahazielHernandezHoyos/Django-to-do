e4da8725d1035ac45a2e539568f6630765007df1
Svet01 <sebastianvilte14@gmail.com>
2022-08-28 00:13:15 -0300

    hola jojos

--- crud/views.py
+++ crud/views.py
@@ -4,9 +4,15 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def task(request):
-    dictionary = {
-        "id": 0,
-        "estado": "por hacer",
-        "checklist": ["Coding", "Art", "Gaming", "Cricket", "Piano"]
-    }
-    return JsonResponse(dictionary)
+    metodo = request.method
+    if metodo == "GET":
+        dictionary = {
+            "id": 0,
+            "estado": "por hacer",
+            "checklist": ["Coding", "Art", "Gaming", "Cricket", "Piano"]
+        }
+        return JsonResponse(dictionary)
+    if metodo == "POST":
+        body = request.body
+        print(request.body)
+        return JsonResponse(body)
