be365a0d252c976a97b76150d8c4d7938b3878e0
JahazieHernandezHoyos <jahazielhernandezhoyoz@gmail.com>
2022-08-27 21:45:37 -0500

    Added dictionary and library JsonResponse

--- crud/views.py
+++ crud/views.py
@@ -1,6 +1,12 @@
 import http
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def task(request):
-    return HttpResponse("hola jojos")
+    dictionary = {
+        "id": 0,
+        "estado": "por hacer",
+        "checklist": ["Coding", "Art", "Gaming", "Cricket", "Piano"]
+    }
+    return JsonResponse(dictionary)
